9f6b5ade5058cc6334e943bd2e5529d06c09bf9d
Hunnyishere <yangruoyao@wustl.edu>
2020-11-28 20:14:06 -0600

    show create action form in modal

--- static/js/addAction.js
+++ static/js/addAction.js
@@ -1,11 +1,12 @@
 function addAction(){
     $("#new_action_button").click(function(){
+        $('#newActionModal').modal('toggle');
         addActionSetup();
     });
 }
 
 function addActionSetup(){
-    $("#create_action_layout").empty();
+    $("#newActionModalBody").empty();
     loadNewActionBody();
     loadNewActionObj();
     $("#new-action-name-select").change(function(){
@@ -13,7 +14,8 @@ function addActionSetup(){
     });
 
     // submit add new action
-    $("#submit_add_action_button").click(function(){
+    $("#submitNewActionModal").click(function(){
+        $('#newActionModal').modal('toggle');
         let action_idx = $("#new-action-name-select option:selected").val();
         let action = action_selects[action_idx];
         let new_action = [action[0]];
@@ -27,7 +29,7 @@ function addActionSetup(){
 
 function createVirtualNewAction(new_action){
     let action_str = new_action.join(', ');
-    $("<div class='new-action-sortable'><div class='new-action-div'><h4 class='action-text ml-3'>"+action_str+"</h4></div></div>").appendTo("#new_action_layout");
+    $("<div class='new-action-sortable'><div class='new-action-div mb-5'><h4 class='action-text ml-3'>"+action_str+"</h4></div></div>").appendTo("#new_action_layout");
     $(".new-action-sortable").sortable({
         connectWith: "#planning",
         stop: function(event, div){
@@ -44,7 +46,7 @@ function createVirtualNewAction(new_action){
                     loadInitialSetting();
                     createPlanning();
                     // clear add action layout
-                    $("#create_action_layout").empty();
+                    $("#newActionModalBody").empty();
                 },
                 dataType: "json"
             });
@@ -55,10 +57,8 @@ function createVirtualNewAction(new_action){
 }
 
 function loadNewActionBody(){
-    $("<h3 class='big-title-text'>New Action</h3><div id='create_action_form' class='form-group-lg'></div>").appendTo("#create_action_layout");
-    $("<br/><button class='btn btn-primary' id='submit_add_action_button'>Submit</button>").appendTo("#create_action_form");
-    let name_label = $("<label for='new-action-name-select' id='new-action-name-label'><h4 class='title-text'>action-name</h4></label>").insertBefore("#submit_add_action_button");
-    let name_select = $("<select class='form-control' id='new-action-name-select'></select>").insertBefore("#submit_add_action_button");
+    let name_label = $("<label for='new-action-name-select' id='new-action-name-label'><h4 class='title-text'>action-name</h4></label>").appendTo("#newActionModalBody");
+    let name_select = $("<select class='form-control' id='new-action-name-select'></select>").appendTo("#newActionModalBody");
     for(let act_idx in action_selects){
         let act = action_selects[act_idx];
         let name = act[0];
@@ -68,18 +68,16 @@ function loadNewActionBody(){
 
 function loadNewActionObj(){
     // remove all previous obj selects
-    $("#create_action_form >select[id!='new-action-name-select']").remove();
-    $("#create_action_form >label[id!='new-action-name-label']").remove();
-    $("#create_action_form >br").remove();
-    $("<br/>").insertBefore("#submit_add_action_button");
+    $("#newActionModalBody >select[id!='new-action-name-select']").remove();
+    $("#newActionModalBody >label[id!='new-action-name-label']").remove();
     // create new ones according to selected action
     let selected_idx = $("#new-action-name-select option:selected").val();
     let selected_act = $("#new-action-name-select option:selected").text();
     for(let obj_idx=1; obj_idx<Object.keys(action_selects[selected_idx]).length;obj_idx++){
         let obj = action_selects[selected_idx][obj_idx];
         let obj_type = action_params[selected_act][obj];
-        let obj_label = $("<label for='"+obj_type+"'><h4 class='title-text'>"+obj_type+' - '+obj+"</h4></label>").insertBefore("#submit_add_action_button");
-        let obj_select = $("<select class='new-action-obj-"+obj_idx+" form-control' id='"+obj_type+"'></select><br/>").insertBefore("#submit_add_action_button");
+        let obj_label = $("<label for='"+obj_type+"'><h4 class='title-text'>"+obj_type+' - '+obj+"</h4></label>").appendTo("#newActionModalBody");
+        let obj_select = $("<select class='new-action-obj-"+obj_idx+" form-control' id='"+obj_type+"'></select>").appendTo("#newActionModalBody");
         for(let obj_name_idx in objects[obj_type]){
             let obj_name = objects[obj_type][obj_name_idx];
             obj_select.append($("<option>").attr("value",obj_name_idx).text(obj_name));
--- static/js/addPreEff.js
+++ static/js/addPreEff.js
@@ -1,7 +1,7 @@
 function addPreOrEff(){
     $(".pos-pre_add_button").each(function(){
         $(this).click(function(){
-            $("#create_precondition_layout").empty();
+            $("#newPreconditionModalBody").empty();
             $(".pos-pre-btn, .neg-pre-btn, .pos-eff-btn, .neg-eff-btn").removeClass("btn-danger");
             $(".pos-pre_add_button, .neg-pre_add_button, .pos-eff_add_button, .neg-eff_add_button").removeClass("btn-danger");
             $(this).addClass("btn-danger");
@@ -12,7 +12,7 @@ function addPreOrEff(){
 
     $(".neg-pre_add_button").each(function(){
         $(this).click(function(){
-            $("#create_precondition_layout").empty();
+            $("#newPreconditionModalBody").empty();
             $(".pos-pre-btn, .neg-pre-btn, .pos-eff-btn, .neg-eff-btn").removeClass("btn-danger");
             $(".pos-pre_add_button, .neg-pre_add_button, .pos-eff_add_button, .neg-eff_add_button").removeClass("btn-danger");
             $(this).addClass("btn-danger");
@@ -23,7 +23,7 @@ function addPreOrEff(){
 
     $(".pos-eff_add_button").each(function(){
         $(this).click(function(){
-            $("#create_precondition_layout").empty();
+            $("#newPreconditionModalBody").empty();
             $(".pos-pre-btn, .neg-pre-btn, .pos-eff-btn, .neg-eff-btn").removeClass("btn-danger");
             $(".pos-pre_add_button, .neg-pre_add_button, .pos-eff_add_button, .neg-eff_add_button").removeClass("btn-danger");
             $(this).addClass("btn-danger");
@@ -34,7 +34,7 @@ function addPreOrEff(){
 
     $(".neg-eff_add_button").each(function(){
         $(this).click(function(){
-            $("#create_precondition_layout").empty();
+            $("#newPreconditionModalBody").empty();
             $(".pos-pre-btn, .neg-pre-btn, .pos-eff-btn, .neg-eff-btn").removeClass("btn-danger");
             $(".pos-pre_add_button, .neg-pre_add_button, .pos-eff_add_button, .neg-eff_add_button").removeClass("btn-danger");
             $(this).addClass("btn-danger");
@@ -45,7 +45,8 @@ function addPreOrEff(){
 }
 
 function addPreSetup(target_name, act_idx){
-    $("#create_pre_layout").empty();
+    $('#newPreconditionModal').modal('toggle');
+    $("#newPreconditionModalBody").empty();
     loadNewPreBody();
     loadNewPreObj();
     $("#new-precondition-name-select").change(function(){
@@ -53,7 +54,8 @@ function addPreSetup(target_name, act_idx){
     });
 
     // submit add new pre
-    $("#submit_add_precondition_button").click(function(){
+    $("#submitNewPreconditionModal").click(function(){
+        $('#newPreconditionModal').modal('toggle');
         let pre_idx = $("#new-precondition-name-select option:selected").val();
         let pre = pre_selects[pre_idx];
         let new_pre = [pre[0]];
@@ -72,7 +74,7 @@ function addPreSetup(target_name, act_idx){
                 loadInitialSetting();
                 createPlanning();
                 // clear add precondition layout
-                $("#create_precondition_layout").empty();
+                $("#newPreconditionModalBody").empty();
                 $(".pos-pre_add_button, .neg-pre_add_button, .pos-eff_add_button, .neg-eff_add_button").removeClass("btn-danger");
             },
             dataType: "json"
@@ -81,10 +83,8 @@ function addPreSetup(target_name, act_idx){
 }
 
 function loadNewPreBody(){
-    $("<h3 class='big-title-text'>New Precondition</h3><div id='create_precondition_form' class='form-group-lg'></div>").appendTo("#create_precondition_layout");
-    $("<br/><button class='btn btn-primary' id='submit_add_precondition_button'>Submit</button>").appendTo("#create_precondition_form");
-    let name_label = $("<label for='new-precondition-name-select' id='new-precondition-name-label'><h4 class='title-text'>precondition-name</h4></label>").insertBefore("#submit_add_precondition_button");
-    let name_select = $("<select id='new-precondition-name-select' class='form-control'></select>").insertBefore("#submit_add_precondition_button");
+    let name_label = $("<label for='new-precondition-name-select' id='new-precondition-name-label'><h4 class='title-text'>precondition-name</h4></label>").appendTo("#newPreconditionModalBody");
+    let name_select = $("<select id='new-precondition-name-select' class='form-control'></select>").appendTo("#newPreconditionModalBody");
     for(let pre_idx in pre_selects){
         let pre = pre_selects[pre_idx];
         let name = pre[0];
@@ -94,18 +94,16 @@ function loadNewPreBody(){
 
 function loadNewPreObj(){
     // remove all previous obj selects
-    $("#create_precondition_form >select[id!='new-precondition-name-select']").remove();
-    $("#create_precondition_form >label[id!='new-precondition-name-label']").remove();
-    $("#create_precondition_form >br").remove();
-    $("<br/>").insertBefore("#submit_add_precondition_button");
+    $("#newPreconditionModalBody >select[id!='new-precondition-name-select']").remove();
+    $("#newPreconditionModalBody >label[id!='new-precondition-name-label']").remove();
     // create new ones according to selected precondition
     let selected_idx = $("#new-precondition-name-select option:selected").val();
     let selected_pre = pre_selects[selected_idx];
     for(let obj_idx=1; obj_idx<Object.keys(pre_selects[selected_idx]).length;obj_idx++){
         let obj = pre_selects[selected_idx][obj_idx];
         let obj_type = pre_params[selected_pre.join(';')][obj];
-        let obj_label = $("<label for='"+obj_type+"'><h4 class='title-text'>"+obj_type+' - '+obj+"</h4></label>").insertBefore("#submit_add_precondition_button");
-        let obj_select = $("<select class='new-precondition-obj-"+obj_idx+" form-control' id='"+obj_type+"'></select><br/>").insertBefore("#submit_add_precondition_button");
+        let obj_label = $("<label for='"+obj_type+"'><h4 class='title-text'>"+obj_type+' - '+obj+"</h4></label>").appendTo("#newPreconditionModalBody");
+        let obj_select = $("<select class='new-precondition-obj-"+obj_idx+" form-control' id='"+obj_type+"'></select>").appendTo("#newPreconditionModalBody");
         for(let obj_name_idx in objects[obj_type]){
             let obj_name = objects[obj_type][obj_name_idx];
             obj_select.append($("<option>").attr("value",obj_name_idx).text(obj_name));
